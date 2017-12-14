77eaf9c0cefb914f90cb437968231e018c04428e
destroyedlolo <l_faillie@yahoo.com>
2017-12-14 23:22:51 +0100

    Add ds18b20 reference probe reading

--- DHT22/DHT22.ino
+++ DHT22/DHT22.ino
@@ -1,6 +1,7 @@
 /* Test d'acquisition de température & d'humidité par DHT22
  *
  *	13/12/2017 - Première version
+ *	14/12/2017 - Ajoute du code pour étaloner la sonde
  *
  *  Par Destroyedlolo, mis dans le Domaine Publique.
  *	Les explications sur le code : http://destroyedlolo.info/ESP/DHT22/
@@ -9,6 +10,13 @@
 
 SimpleDHT22 DHT;
 int pinDHT = 5;	// Broche sur laquelle est connecté le DHT22
+
+#include <OWBus.h>
+#include <OWBus/DS18B20.h>
+
+OneWire oneWire(2);
+OWBus bus(&oneWire);
+
 
 void setup(){
 	Serial.begin(115200);	// débuging
@@ -29,5 +37,9 @@ void loop(){
 		Serial.print(humidite); Serial.println(" RH%");
 	}
 
-	ESP.deepSleep(3 * 1e6);
+	DS18B20 sonde( bus, 0x28A7A073070000AE );
+	float t = sonde.getTemperature() - 0.0208544495;
+	Serial.println( t );
+
+	ESP.deepSleep(30 * 1e6);
 }
